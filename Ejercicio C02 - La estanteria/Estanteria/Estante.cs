e8df663596cf48891e85b1f29114a8c063beef59
Belen <belenn2206@gmail.com>
2022-09-05 22:34:19 -0300

    Ejercicio Terminado

---
+++ Ejercicio C02 - La estanteria/Estanteria/Estante.cs
@@ -0,0 +1,104 @@
+﻿using System.Text;
+
+namespace Ejercicio_C02___La_estanteria.Estanteria
+{
+    internal class Estante
+    {
+        private Producto[] _productos;
+        private int _ubicacionEstante;
+
+        private Estante(int capacidad)
+        {
+            _productos = new Producto[capacidad];
+        }
+
+        public Estante(int capacidad, int ubicacion) : this(capacidad)
+        {
+            _ubicacionEstante = ubicacion;
+        }
+
+        public Producto[] GetProducto()
+        {
+            return _productos;
+        }
+
+        static public string MostrarEstante(Estante e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Estante n° " + e._ubicacionEstante);
+
+            for (int i = 0; i < e._productos.Length; i++)
+            {
+                sb.AppendLine(Producto.MostrarProducto(e._productos[i]).ToString());
+            }
+            return sb.ToString();
+        }
+
+        #region SOBRECARGAS
+
+        public static bool operator ==(Estante e, Producto p)
+        {
+            bool result = false;
+            for (int i = 0; i < e._productos.Length; i++)
+            {
+                if (e._productos[i] == p)
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+        public static bool operator !=(Estante e, Producto p)
+        {
+            bool result = true;
+            for (int i = 0; i < e._productos.Length; i++)
+            {
+                if ((e._productos[i] == p))
+                {
+                    result = false;
+                }
+            }
+            return result;
+        }
+
+        public static bool operator +(Estante e, Producto p)
+        {
+            bool retorno = false;
+            int len = e._productos.Length;
+            if (e != p)
+            {
+                for (int i = 0; i < len; i++)
+                {
+
+                    if (e._productos[i] is null)
+                    {
+                        e._productos[i] = p;
+                        retorno = true;
+                        break;
+                    }
+
+                }
+            }
+                
+            return retorno;
+        }
+
+        public static Estante operator -(Estante e, Producto p)
+        {
+            Producto buffer = new Producto("", "", 0);
+            int len = e._productos.Length;
+            for (int i = 0; i < len; i++)
+            {
+                if(e._productos[i] == p)
+                {
+                    e._productos[i] = buffer;
+                }
+            }
+            return e;
+        }
+
+        #endregion
+
+
+    }
+}
---
+++ Ejercicio C02 - La estanteria/Estanteria/Producto.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_C02___La_estanteria.Estanteria
+{
+    internal class Producto
+    {
+        private string _codigoDeBarra;
+        private string _marca;
+        private float _precio;
+
+        public Producto(string marca, string codigo, float precio)
+        {
+            this._codigoDeBarra = codigo;
+            this._marca = marca;
+            this._precio = precio;
+        }
+
+        public string GetMarca()
+        {
+            return this._marca;
+        }
+        
+        public float GetPrecio()
+        {
+            return this._precio;
+        }
+
+        static public string MostrarProducto(Producto p)
+        {
+            StringBuilder sb = new StringBuilder();
+            if(p is not null)
+            {
+                sb.AppendLine("Marca: " + p._marca);
+                sb.AppendLine("Precio: " + p._precio);
+                sb.AppendLine("Codigo: " + p._codigoDeBarra.ToString());
+            }
+            
+
+            return sb.ToString();
+
+        }
+
+        #region SOBRECARGAS
+
+        public static explicit operator string(Producto p)
+        {
+            return p._codigoDeBarra;
+        }
+
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            bool result = false;
+            if(p1 is not null && p2 is not null)
+            {
+                if (p1._marca == p2._marca)
+                {
+                    if (p1._codigoDeBarra == p2._codigoDeBarra)
+                    {
+                        result = true;
+                    }
+                }
+            }
+            
+            return result;
+        }
+
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            bool result = true;
+            if (p1 is null && p2 is null || ((p1._marca == p2._marca) && (p1._codigoDeBarra == p2._codigoDeBarra)))
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        public static bool operator ==(Producto p, string marca)
+        {
+            bool result = false;
+            if (p._marca == marca)
+            {
+                result = true;
+            }
+            return result;
+        }
+
+        public static bool operator !=(Producto p, string marca)
+        {
+            bool result = false;
+            if (!(p._marca == marca))
+            {
+                result = true;
+            }
+            return result;
+        }
+
+        #endregion
+
+    }
+}
